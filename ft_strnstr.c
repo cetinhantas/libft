cbe89a2d6a126466414f5e191849c543e8bec659
cetinhantas <127848030+cetinhantas@users.noreply.github.com>
2025-06-11 20:05:43 +0300

    index update fix

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -18,11 +18,11 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	j;
 
 	i = 0;
-	j = 0;
 	if (!*little)
 		return ((char *)big);
 	while (big[i])
 	{
+		j = 0;
 		while (big[i + j] == little[j] && i + j < len)
 		{
 			if (!little[j + 1])
